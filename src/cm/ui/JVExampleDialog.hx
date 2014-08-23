721ca9c3959b3925038021c7ffcc6d1781a67617
Fletcher <fletcherc89@gmail.com>
2014-08-22 23:47:48 -0700

    Main menu visuals

--- src/cm/ui/JVExampleDialog.hx
+++ src/cm/ui/JVExampleDialog.hx
@@ -1,11 +1,13 @@
 package cm.ui;
 
+import flash.geom.Point;
 import com.haxepunk.graphics.Image;
 import extendedhxpunk.ui.UIImageView;
 import extendedhxpunk.ui.UIView;
 import extendedhxpunk.ext.EXTUtility;
 import extendedhxpunk.ext.EXTOffsetType;
-import flash.geom.Point;
+import cm.local.CMLocalData;
+import cm.local.CMColorPalette;
 
 /**
  * JVExampleDialog
@@ -19,39 +21,41 @@ class JVExampleDialog extends UIView
 		super(position, size);
 		
 		// Sides
-		var sideImageHorizontal:Image = new Image("gfx/ui/speech_bubble_side_horizontal_8x8.png");
-		sideImageHorizontal.scaledWidth = size.x;
-		var sideImageVertical:Image = new Image("gfx/ui/speech_bubble_side_vertical_8x8.png");
-		sideImageVertical.scaledHeight = size.y;
-		var topImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageHorizontal);
-		topImageView.offsetAlignmentInParent = EXTOffsetType.TOP_CENTER;
-		var rightImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageVertical);
-		rightImageView.offsetAlignmentInParent = EXTOffsetType.RIGHT_CENTER;
-		var botImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageHorizontal);
-		botImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_CENTER;
-		var leftImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageVertical);
-		leftImageView.offsetAlignmentInParent = EXTOffsetType.LEFT_CENTER;
+		// var sideImageHorizontal:Image = new Image("gfx/ui/speech_bubble_side_horizontal_8x8.png");
+		// sideImageHorizontal.scaledWidth = size.x;
+		// var sideImageVertical:Image = new Image("gfx/ui/speech_bubble_side_vertical_8x8.png");
+		// sideImageVertical.scaledHeight = size.y;
+		// var topImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageHorizontal);
+		// topImageView.offsetAlignmentInParent = EXTOffsetType.TOP_CENTER;
+		// var rightImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageVertical);
+		// rightImageView.offsetAlignmentInParent = EXTOffsetType.RIGHT_CENTER;
+		// var botImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageHorizontal);
+		// botImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_CENTER;
+		// var leftImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, sideImageVertical);
+		// leftImageView.offsetAlignmentInParent = EXTOffsetType.LEFT_CENTER;
 		
 		// Corners
-		var cornerImage:Image = new Image("gfx/ui/speech_bubble_corner_8x8.png");
-		var upLeftCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
-		upLeftCornerImageView.offsetAlignmentInParent = EXTOffsetType.TOP_LEFT;
-		var upRightCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
-		upRightCornerImageView.offsetAlignmentInParent = EXTOffsetType.TOP_RIGHT;
-		var botRightCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
-		botRightCornerImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_RIGHT;
-		var botLeftCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
-		botLeftCornerImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_LEFT;
+		// var cornerImage:Image = new Image("gfx/ui/speech_bubble_corner_8x8.png");
+		// var upLeftCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
+		// upLeftCornerImageView.offsetAlignmentInParent = EXTOffsetType.TOP_LEFT;
+		// var upRightCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
+		// upRightCornerImageView.offsetAlignmentInParent = EXTOffsetType.TOP_RIGHT;
+		// var botRightCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
+		// botRightCornerImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_RIGHT;
+		// var botLeftCornerImageView:UIImageView = new UIImageView(EXTUtility.ZERO_POINT, cornerImage);
+		// botLeftCornerImageView.offsetAlignmentInParent = EXTOffsetType.BOTTOM_LEFT;
 		
 		// Set up the subviews
-		this.backgroundColor.setColor(0, 0, 0, 0.8);
-		this.addSubview(topImageView);
-		this.addSubview(rightImageView);
-		this.addSubview(botImageView);
-		this.addSubview(leftImageView);
-		this.addSubview(upLeftCornerImageView);
-		this.addSubview(upRightCornerImageView);
-		this.addSubview(botRightCornerImageView);
-		this.addSubview(botLeftCornerImageView);
+		this.backgroundColor.webColor = CMLocalData.sharedInstance().currentColorPalette.colorForIndex(CMColorPalette.INDEX_BACKGROUND_2).webColor;
+		this.backgroundColor.alpha = 1.0;
+		// this.backgroundColor.setColor(0, 0, 0, 0.8);
+		// this.addSubview(topImageView);
+		// this.addSubview(rightImageView);
+		// this.addSubview(botImageView);
+		// this.addSubview(leftImageView);
+		// this.addSubview(upLeftCornerImageView);
+		// this.addSubview(upRightCornerImageView);
+		// this.addSubview(botRightCornerImageView);
+		// this.addSubview(botLeftCornerImageView);
 	}
 }
